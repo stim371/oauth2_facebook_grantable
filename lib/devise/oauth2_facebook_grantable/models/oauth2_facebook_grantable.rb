0230fb4baed01ea16327a704c2180035362e0ae3
Joel Stimson <stimsojr@gmail.com>
2014-06-10 22:51:08 -0700

    change to use omniauth-facebook uid column.

--- lib/devise/oauth2_facebook_grantable/models/oauth2_facebook_grantable.rb
+++ lib/devise/oauth2_facebook_grantable/models/oauth2_facebook_grantable.rb
@@ -6,7 +6,7 @@ module Devise
           fb_user = Devise::Oauth2ProvidableFacebook.facebook_user_for_token(token)
           if(fb_user && fb_user["id"])
             Devise::Oauth2ProvidableFacebook.logger.debug("Oauth2FacebookGrantable => User with facebook identifier \"#{fb_user["id"]}\" was authenticated successfully by Facebook")
-            return (fb_user["id"].to_s == self.facebook_identifier.to_s)
+            return (fb_user["id"].to_s == self.uid.to_s)
           else
             Devise::Oauth2ProvidableFacebook.logger.debug("Oauth2FacebookGrantable => Could not authenticate user against Facebook (#{fb_user})")
             return false
